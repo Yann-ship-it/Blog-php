fa2c5faf35a7f3f67308dcae1ca36e1c3f8e8b6d
Yann-ship-it <yy.iris85@gmail.com>
2023-03-20 12:07:53 +0100

    Correction single

--- comp/header.php
+++ comp/header.php
@@ -3,8 +3,8 @@
       <h1 class="logo-text"><span>Ron</span>asdev</h1>
     </div>
     <ul class="nav">
-      <li><a href="./index.php">Accueil</a></li>
-      <li><a href="/index.php">Admin</a></li>
+      <li><a href="/Blog/index.php">Accueil</a></li>
+      <li><a href="/Blog/posts/index.php">Admin</a></li>
       <li><a href="#">Services</a></li>
       <li><a href="#">S'inscrire</a></li>
       <li><a href="#">Se connecter</a></li> 
